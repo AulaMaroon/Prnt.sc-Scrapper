cb153b96f5b22b607952c3b79eff9a63f1330514
AulaMaroon <kadekfp@gmail.com>
2022-01-04 20:48:44 +0800

    added scrapper without using base64

--- ImageScrapperWOB64.py
+++ ImageScrapperWOB64.py
@@ -18,27 +18,14 @@ def loaderandgrabber():
     url = "https://prnt.sc/" + result_str
     driver.get(url)
     os.system('cls')
-    ss = driver.find_element(By.XPATH, '/html/body/div[3]/div/div/img')
-    img_url = ss.get_attribute('src')
-    base64Img = img_url.replace("data:image/png;base64,", "")
     spliturl = url.rsplit('/', 1)[-1]
-    return base64Img, spliturl
+    return spliturl
 
-def downloader_image(download_path, url, file_name):
-    sample = base64.b64decode(url)
-    for tf in imghdr.tests:
-        res = tf(sample, None)
-        if res:
-            break
-    if res == None:
-        fname = download_path + file_name + ".png"
-        elenemt = driver.find_element(By.CLASS_NAME, "screenshot-image")
-        elenemt.screenshot(fname)
-    else:
-        imgdata = base64.b64decode(url) 
-        path = download_path + file_name + "." + res
-        with open(path, 'wb') as f:
-            f.write(imgdata)
+def downloader_image(download_path, file_name):
+    fname = download_path + file_name + ".png"
+    elenemt = driver.find_element(By.CLASS_NAME, "screenshot-image")
+    elenemt.screenshot(fname)
+
     
 def starter():
     time.sleep(5)
@@ -48,8 +35,8 @@ def starter():
     totaldownloaded = 1
     pbar = tqdm(total=amount)
     while totaldownloaded <= amount:
-        url, spliturl = loaderandgrabber()
-        downloader_image("images/", url, spliturl)
+        spliturl = loaderandgrabber()
+        downloader_image("images/", spliturl)
         totaldownloaded += 1
         print("Downloading Images")
         print()
